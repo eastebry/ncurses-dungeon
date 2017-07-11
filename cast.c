d1012e80a8ff9fad07a38d1e4436a11cd6297f06
Bryan Eastes <eastebry@users.noreply.github.com>
2017-07-10 22:12:57 -0700

    Free movement is working, now just a few bugs to fix

--- cast.c
+++ cast.c
@@ -9,6 +9,7 @@ https://www.viget.com/articles/game-programming-in-c-with-the-ncurses-library
 
 #include "player.h"
 
+
 WINDOW* main_window;
 WINDOW* map_window;
 
@@ -45,80 +46,80 @@ void drawColumn(int column, double colHeight, int screenHeight, char character){
 void raycast(struct Player *player, struct Map *map, int width, int height){
   for (int x = 0; x < width; x++){
     //calculate ray position and direction
-      double cameraX = 2 * x / ((double) (width)) - 1; //x-coordinate in camera space
-      double rayPosX = player->x;
-      double rayPosY = player->y;
-      double rayDirX = cos(player->direction) + player->cameraPlaneX * cameraX;
-      double rayDirY = sin(player->direction) + player->cameraPlaneY * cameraX;
+    double cameraX = 2 * x / ((double) (width)) - 1; //x-coordinate in camera space
+    double rayPosX = player->x;
+    double rayPosY = player->y;
+    double rayDirX = cos(player->direction) + player->cameraPlaneX * cameraX;
+    double rayDirY = sin(player->direction) + player->cameraPlaneY * cameraX;
 
-      int mapX = (int) rayPosX;
-      int mapY = (int) rayPosY;
+    int mapX = (int) rayPosX;
+    int mapY = (int) rayPosY;
 
-      //length of ray from current position to next x or y-side
-      double sideDistX;
-      double sideDistY;
+    //length of ray from current position to next x or y-side
+    double sideDistX;
+    double sideDistY;
 
       //length of ray from one x or y-side to next x or y-side
-     double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
-     double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
-     double perpWallDist;
+      double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+      double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+      double perpWallDist;
 
-     //what direction to step in x or y-direction (either +1 or -1)
-     int stepX;
-     int stepY;
+      //what direction to step in x or y-direction (either +1 or -1)
+      int stepX;
+      int stepY;
 
-     char wall = ' '  ;// the character in the map to draw the wall
-     int hit = 0; //was there a wall hit?
-     int side; //was a NS or a EW wall hit?
+      char wall = ' '  ;// the character in the map to draw the wall
+      int hit = false; //was there a wall hit?
+      bool outside = false; // are we outside the map?
+      int side; //was a NS or a EW wall hit?
 
-     //calculate step and initial sideDist
-     if (rayDirX < 0)
-     {
+      //calculate step and initial sideDist
+      if (rayDirX < 0) {
        stepX = -1;
        sideDistX = (rayPosX - mapX) * deltaDistX;
-     }
-     else
-     {
+      }
+      else {
        stepX = 1;
        sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
-     }
-     if (rayDirY < 0)
-     {
+      }
+      if (rayDirY < 0) {
        stepY = -1;
        sideDistY = (rayPosY - mapY) * deltaDistY;
-     }
-     else
-     {
+      }
+      else {
        stepY = 1;
        sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
-     }
-     //perform DDA
-     while (hit == 0)
-     {
+      }
+      //perform DDA
+      while (!hit && !outside) {
        //jump to next map square, OR in x-direction, OR in y-direction
-       if (sideDistX < sideDistY)
-       {
+       if (sideDistX < sideDistY) {
          sideDistX += deltaDistX;
          mapX += stepX;
          side = 0;
        }
-       else
-       {
+       else {
          sideDistY += deltaDistY;
          mapY += stepY;
          side = 1;
        }
+       if (mapX < 0 || mapY < 0 || mapY > map->size || mapY > map->size) {
+         outside = true;
+       }
        //Check if ray has hit a wall
        wall = getPositionInMap(mapY, mapX, map);
-       if (wall != ' ') hit = 1;
-     }
-     //Calculate distance projected on camera direction (oblique distance will give fisheye effect!)
-     if (side == 0) perpWallDist = (mapX - rayPosX + (1 - stepX) / 2) / rayDirX;
-     else           perpWallDist = (mapY - rayPosY + (1 - stepY) / 2) / rayDirY;
+       if (wall != ' ') hit = true;
+      }
 
-     //Calculate height of line to draw on screen
-     int lineHeight = (int)(height / perpWallDist);
-     drawColumn(x, lineHeight, height, wall);
+      //Calculate distance projected on camera direction (oblique distance will give fisheye effect!)
+      if (hit) {
+         if (side == 0) perpWallDist = (mapX - rayPosX + (1 - stepX) / 2) / rayDirX;
+         else           perpWallDist = (mapY - rayPosY + (1 - stepY) / 2) / rayDirY;
+
+         //Calculate height of line to draw on screen
+         int lineHeight = (int)(height / perpWallDist);
+         drawColumn(x, lineHeight, height, wall);
+     }
    }
  }
 
@@ -139,6 +140,9 @@ void drawMiniMap(struct Player *player, struct Map *map){
   char str3[80];
   sprintf(str3, "Direction: %f", player->direction);
   mvprintw(20, offset, &str3);
+  char str4[80];
+  sprintf(str4, "Time: %d", (int)time(NULL));
+  mvprintw(30, offset, &str4);
 }
 
 void update(struct Player *player, struct Map *map, int w, int h){
@@ -151,9 +155,9 @@ void update(struct Player *player, struct Map *map, int w, int h){
 void walkAnimation(struct Player *player, struct Map *map, double distance, double direction){
   const struct timespec delay = {0, 90000000L};
   double moveSpeed = .1;
-  double finalX = cos(player->direction) * distance * direction;
-  double finalY = sin(player->direction) * distance * direction;
-  for (double i = 0; i < 10/*distance/moveSpeed*/; i+=1){
+  double finalX = floor(player-> x + cos(player->direction) * distance * direction);
+  double finalY = floor(player-> y + sin(player->direction) * distance * direction);
+  for (double i = 0; i <(int) floor(distance/moveSpeed); i+=1){
     walk(player, moveSpeed, direction);
     update(player, map, 200, 60);
     nanosleep(&delay, NULL);
@@ -166,7 +170,7 @@ void rotationAnimation(struct Player *player, struct Map *map, double radians, i
   double moveSpeed = 0.1;
   const struct timespec delay = {0, 90000000L};
   double finalDirection = player->direction + (radians * direction);
-  for (double i=0; i < fabs(radians/moveSpeed); i+=1){
+  for (double i=0; i < (int) floor(fabs(radians/moveSpeed)); i+=1){
     rotate(player, moveSpeed, direction);
     // TODO -factor this call to update out somehow
     update(player, map, 200, 60);
@@ -190,19 +194,15 @@ int main(){
   while (1){
     char input = getch();
     switch(input) {
-      case KEY_UP:
       case 'w':
         walkAnimation(&player, &map, 1, PLAYER_FORWARDS);
         break;
-      case KEY_DOWN:
       case 's':
         walkAnimation(&player, &map, 1, PLAYER_BACKWARDS);
         break;
-      case KEY_LEFT:
       case 'a':
         rotationAnimation(&player, &map, M_PI/2.0, PLAYER_COUNTER_CLOCKWISE);
         break;
-      case KEY_RIGHT:
       case 'd':
         rotationAnimation(&player, &map, M_PI/2.0, PLAYER_CLOCKWISE);
         break;
